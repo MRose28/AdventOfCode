fe1c413eb4fb479b8c434d6e8b48732d06d4126d
Mathis Rose <mathis.rose@worldiety.de>
2020-12-25 14:43:13 +0100

    Solution to day25 part 1.

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"mrose.de/aoc/2020/day22"
+	"mrose.de/aoc/2020/day25"
 	"time"
 )
 
 //Solve the puzzle
 func main() {
 	defer elapsed("calculation")()
-	result := day22.Solve()
+	result := day25.Solve()
 	fmt.Printf("\n\n%d\n\n", result)
 }
 
